57438574c2ef690228990cd6cf5788604ce72465
dumadoniagara <dumadsambora@gmail.com>
2020-12-21 11:28:06 +0700

    first commit

---
+++ sandbox.ts
@@ -0,0 +1,15 @@
+// cara nulisnya sama dengan javascript 
+// cara compile nya tsc <nama file .ts> <nama file .js>
+// kalo misalnya nama filenya sama cukup menuliskan yang .ts nya aja.
+// kalo ga ada file .js dia akan membuat extension .js nya
+
+// biar ga harus execute manual setiap ada perubahan lakukan watch monitoring file .ts nya :
+// co : sandbox.ts -w
+var character = "mikha";
+console.log('character ==', character);
+var inputs = document.querySelectorAll('input');
+console.log('inputs ==', inputs);
+
+inputs.forEach(input => {
+   console.log(input);
+});
