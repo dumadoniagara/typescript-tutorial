b9e5b4c03081cebd3e1aa002dbb4b1509aa07f2a
dumadoniagara <dumadsambora@gmail.com>
2020-12-24 15:51:24 +0700

    array and object

--- sandbox.js
+++ sandbox.js
@@ -1,11 +1,8 @@
-// assigning allowable data types.
-// typescript hanya membolehkan re-assigning variable dengan jenis data type yang sejenis.
-var character = "luigi";
-var age = 40;
-var isBlackBelt = true;
-isBlackBelt = false;
-// ngasih tau kalo diameter tuh jenis data yang diperbolehkannya hanya number.
-var circ = function (diameter) {
-    return diameter * Math.PI;
-};
-console.log(circ(7.5));
+// tipe data array dan object manjadi lebih strict pada ts
+// co : array yang memuat semua tipe data string ga akan bisa di assign tipe data lainnya
+// gitu juga object --> harus memuat pasang key-value pair yang sama DAN tipe data dari value masing-masing
+// key akan bersifat sama. 
+// array
+var arr = ['duma', 'doniagara', 'sambora'];
+arr.push('nama'); //valid
+arr.push(3); //ga valid
--- sandbox.ts
+++ sandbox.ts
@@ -1,15 +1,40 @@
-// assigning allowable data types.
-// typescript hanya membolehkan re-assigning variable dengan jenis data type yang sejenis.
+// tipe data array dan object manjadi lebih strict pada ts
+// co : array yang memuat semua tipe data string ga akan bisa di assign tipe data lainnya
+// gitu juga object --> harus memuat pasang key-value pair yang sama DAN tipe data dari value masing-masing
+// key akan bersifat sama. 
 
-let character = "luigi";
-let age = 40;
-let isBlackBelt = true;
+// array
+let arr = ['duma', 'doniagara', 'sambora'];
+arr.push('nama'); //valid
+// arr.push(3); -> ga valid (di error-in)
 
-isBlackBelt = false;
+// kalo nimpa variable jadi tipe data lain juga gabisa.
 
-// ngasih tau kalo diameter tuh jenis data yang diperbolehkannya hanya number.
-const circ = (diameter: number) => {
-   return diameter * Math.PI;
-}
 
-console.log(circ(7.5));
+let mixed = [4, 3, 'duma', 'doniagara'];
+mixed.push(54); //valid
+// mixed.push(true); --> ga valid
+
+
+// object
+let ninja = {
+   name: 'mario',
+   belt: 'black',
+   age: 30,
+};
+
+ninja.age = 40; //--> valid
+// ninja.age = '30'; // --> not valid
+
+// ninja.skills = ['fighting', 'sneaking']; //can't add additional property
+
+// kalo mau nimpa object yang udah ada harus sama strukturnya.
+ 
+
+// gak valid karena ga akan nambah key baru yang sebelumnya ga ada.
+// ninja = {
+//    name: 'yoshi',
+//    belt: 'orange',
+//    age: 40
+//    skills: ['fighting']
+// }
